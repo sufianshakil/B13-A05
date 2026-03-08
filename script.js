761c014863c43a2d853f91d41c9a1e2c05201abb
sufianshakil <shakil.md6@gmail.com>
2026-03-08 12:07:48 +0600

    card create

--- script.js
+++ script.js
@@ -11,3 +11,70 @@ function login(){
     }
 
 }
+
+// LOAD ALL ISSUES
+
+async function loadIssues(){
+
+const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+
+const data = await res.json()
+
+displayIssues(data.data)
+
+};
+
+
+// DISPLAY ISSUES
+
+function displayIssues(issues){
+
+const container = document.getElementById("issueContainer")
+
+const count = document.getElementById("issueCount")
+
+container.innerHTML = ""
+
+count.innerText = issues.length + " Issues"
+
+
+issues.forEach(issue => {
+
+const card = document.createElement("div")
+
+let border = issue.status === "open" ? "border-green-500" : "border-purple-500"
+
+card.innerHTML = `
+
+<div class="card-body card bg-white shadow border-t-4 ${border} cursor-pointer">
+
+<div class="flex justify-between items-center">
+            <img src="./assets/Open-Status.png" alt="">
+            <h2 class="bg-red-100 px-7 py-2 rounded-xl text-green-600 font-semibold uppercase">${issue.priority}</h2>
+        </div>
+
+<h2 class="card-title">${issue.title}</h2>
+
+<p>${issue.description}</p>
+
+<div class="flex gap-2 mt-2">
+
+<span class="badge badge-error">BUG</span>
+
+<span class="badge badge-warning">HELP</span>
+
+</div>
+
+<p class="text-sm mt-3">#${issue.id} by ${issue.author}</p>
+
+<p class="text-sm">${issue.createdAt}</p>
+
+</div>
+
+`
+container.appendChild(card)
+
+})
+
+};
+loadIssues()
